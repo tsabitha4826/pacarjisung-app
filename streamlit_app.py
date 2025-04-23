ef144a5e03ad2e38b496c525ad6b50db98d40eb3
tsabitha4826 <bitha0408@gmail.com>
2025-04-23 14:54:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 pacarjisung")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "ayoo bermain cocok warna bersama pacar jisung, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
